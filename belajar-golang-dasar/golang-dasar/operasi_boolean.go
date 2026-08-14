bb2062ac7811a22242b1a144b81e0ace4b6f0284
Muhammad Rizky Amin <muhammad.amin@MIRACLE-GROUP.LOCAL>
2026-08-14 17:28:44 +0700

    operasi boolean

---
+++ belajar-golang-dasar/golang-dasar/operasi_boolean.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+//operasi &&
+
+//nilai1 | operator | nilai 2 | hasil
+//true | && | true | true
+//true | && | false | false
+//false | && | true | false
+//false | && | false | false
+
+//true | || | true | true
+//true | || | false | true
+//false | || | true | true
+//false | || | false | false
+
+//(!) | || | true | false
+//(!) | || | false | true
+func main(){
+ var nilaiAkhir = 90
+ var absensi = 80
+
+ var lulusNilaiAkhir bool = nilaiAkhir > 80
+ var lulusAbsensi bool = absensi > 80
+
+ var lulus bool =   lulusAbsensi && lulusNilaiAkhir
+
+ fmt.Println(lulus)
+}
