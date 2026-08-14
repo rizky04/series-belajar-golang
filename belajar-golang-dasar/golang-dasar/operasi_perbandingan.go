971d701319fba6d2b0ce78cb1de729d009833c5a
Muhammad Rizky Amin <muhammad.amin@MIRACLE-GROUP.LOCAL>
2026-08-14 14:53:05 +0700

    operasi perbandingan

---
+++ belajar-golang-dasar/golang-dasar/operasi_perbandingan.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+
+//unary operator ++, --, -, + , ! 
+//keterangan a=a+1, a=a-1, negative, psoitive, boolean kebalikasn
+//operasi perbandingan
+// operator >,<,>=,<=,==,!=
+//lebih dari, kurang dari, lebih dari sama dengan, kurang dari sama dengan, sama dengan, tidak sama dengan
+func main(){
+	// var i = 1;
+	//var j =1;
+	// i++
+	// i++
+	//j--
+
+	// fmt.Println(i)
+
+
+	// name1 := "Amin"
+	// name2 := "Amin"
+
+	// var result bool = name1 == name2
+
+	// fmt.Println(result);
+
+
+	// name1 := "Amin"
+	// name2 := "amin"
+
+	// var result bool = name1 == name2
+
+	// fmt.Println(result);
+
+
+	// name1 := "Amin"
+	// name2 := "Amin"
+
+	// var result bool = name1 != name2
+
+	// fmt.Println(result);
+
+
+}
